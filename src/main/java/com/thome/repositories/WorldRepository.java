add46a333a185cd88bf8a173b57dde93d23554af
Hardelele <Hardelele@yahoo.com>
2020-02-21 22:54:50 +0300

    Change id type to UUID

--- src/main/java/com/thome/repositories/WorldRepository.java
+++ src/main/java/com/thome/repositories/WorldRepository.java
@@ -4,6 +4,8 @@ import com.thome.entities.WorldEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface WorldRepository extends MongoRepository<WorldEntity, String> {
+public interface WorldRepository extends MongoRepository<WorldEntity, UUID> {
 }
