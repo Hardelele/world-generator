ef9f3a596f0151935663c0cba89ed01446e16585
Hardelele <Hardelele@yahoo.com>
2020-02-21 20:24:34 +0300

    Implemented full randomly generation of world

--- src/main/java/com/thome/services/GeneratorService.java
+++ src/main/java/com/thome/services/GeneratorService.java
@@ -3,10 +3,11 @@ package com.thome.services;
 import com.google.common.collect.Table;
 import com.thome.models.World;
 import com.thome.utils.TableInstance;
-import javafx.scene.control.TableRow;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class GeneratorService {
@@ -20,21 +21,22 @@ public class GeneratorService {
 
     public World generateNewWorld(String key) {
         Table<Integer, Integer, Integer> worldState = TableInstance.get();
-        constructRandomWorld(worldState);
+        fillWorldStateRandom(worldState);
         return new World(worldState);
     }
 
-    private Table<Integer, Integer, Integer> constructRandomWorld(Table<Integer, Integer, Integer> worldState) {
+    /**
+     * Utils
+     */
+
+    private void fillWorldStateRandom(Table<Integer, Integer, Integer> worldState) {
         Set<Integer> columnKeys = worldState.columnKeySet();
-        columnKeys.forEach(columnKey -> {
-            processingOneColumn(columnKey, worldState);
-            newMethod(worldState.rowMap());
+        Set<Integer> rowKeys = worldState.rowKeySet();
+        columnKeys.forEach(column -> {
+            rowKeys.forEach(row -> {
+                worldState.put(column, row, generateRandomCellValue());
+            });
         });
-        return
-    }
-
-    private void processingOneColumn(Integer columnKey, Table<Integer, Integer, Integer> worldState) {
-
     }
 
     private int generateRandomCellValue() {
