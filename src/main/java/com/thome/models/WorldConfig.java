197670fddedb7df62570c6ee1c7320ea4a14bf56
Hardelele <Hardelele@yahoo.com>
2020-02-21 20:27:50 +0300

    Create WorldConfig DTO

---
+++ src/main/java/com/thome/models/WorldConfig.java
@@ -0,0 +1,16 @@
+package com.thome.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WorldConfig {
+
+    private String name;
+    private int size;
+}
