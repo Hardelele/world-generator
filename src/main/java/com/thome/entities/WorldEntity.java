154767b0e70aa9d6c0fa3d489bfa2005accb63ca
Hardelele <Hardelele@yahoo.com>
2020-02-21 20:28:35 +0300

    Make EqualsAndHashCode callSuper

--- src/main/java/com/thome/entities/WorldEntity.java
+++ src/main/java/com/thome/entities/WorldEntity.java
@@ -3,12 +3,14 @@ package com.thome.entities;
 import com.thome.models.World;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class WorldEntity extends World {
 
     @Id
